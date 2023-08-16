00d9e74c246f4441349c639f8655606a733453bc
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-08-16 21:15:32 +0100

    Initialize do_not_merge/test_clippy

---
+++ crates/turbopack-cli-utils/src/lib.rs
@@ -0,0 +1,14 @@
+#![feature(async_closure)]
+#![feature(min_specialization)]
+#![feature(round_char_boundary)]
+
+pub mod issue;
+pub mod runtime_entry;
+pub mod source_context;
+
+pub fn register() {
+    turbo_tasks::register();
+    turbo_tasks_fs::register();
+    turbopack_core::register();
+    include!(concat!(env!("OUT_DIR"), "/register.rs"));
+}
