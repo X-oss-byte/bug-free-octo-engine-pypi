ff12c1e2953317b11469f9eba6e7161f7ce18372
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-08-16 21:12:19 +0100

    Initialize jankaifer-patch-1

---
+++ cli/internal/runsummary/execution_summary.go
@@ -0,0 +1,239 @@
+package runsummary
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/vercel/turbo/cli/internal/chrometracing"
+	"github.com/vercel/turbo/cli/internal/fs"
+
+	"github.com/mitchellh/cli"
+)
+
+// executionEvent represents a single event in the build process, i.e. a target starting or finishing
+// building, or reaching some milestone within those steps.
+type executionEvent struct {
+	// Timestamp of this event
+	Time time.Time
+	// Duration of this event
+	Duration time.Duration
+	// Target which has just changed
+	Label string
+	// Its current status
+	Status executionEventName
+	// Error, only populated for failure statuses
+	Err error
+}
+
+// executionEventName represents the status of a target when we log a build result.
+type executionEventName int
+
+// The collection of expected build result statuses.
+const (
+	targetBuilding executionEventName = iota
+	TargetBuildStopped
+	TargetBuilt
+	TargetCached
+	TargetBuildFailed
+)
+
+func (en executionEventName) toString() string {
+	switch en {
+	case targetBuilding:
+		return "building"
+	case TargetBuildStopped:
+		return "buildStopped"
+	case TargetBuilt:
+		return "built"
+	case TargetCached:
+		return "cached"
+	case TargetBuildFailed:
+		return "buildFailed"
+	}
+
+	return ""
+}
+
+// TaskExecutionSummary contains data about the state of a single task in a turbo run.
+// Some fields are updated over time as the task prepares to execute and finishes execution.
+type TaskExecutionSummary struct {
+	startAt  time.Time          // set once
+	status   executionEventName // current status, updated during execution
+	err      error              // only populated for failure statuses
+	duration time.Duration      // updated during the task execution
+}
+
+// MarshalJSON munges the TaskExecutionSummary into a format we want
+// We'll use an anonmyous, private struct for this, so it's not confusingly duplicated
+func (ts *TaskExecutionSummary) MarshalJSON() ([]byte, error) {
+	serializable := struct {
+		Start  int64  `json:"startTime"`
+		End    int64  `json:"endTime"`
+		Status string `json:"status"`
+		Err    error  `json:"error"`
+	}{
+		Start:  ts.startAt.UnixMilli(),
+		End:    ts.startAt.Add(ts.duration).UnixMilli(),
+		Status: ts.status.toString(),
+		Err:    ts.err,
+	}
+
+	return json.Marshal(&serializable)
+}
+
+// executionSummary is the state of the entire `turbo run`. Individual task state in `Tasks` field
+type executionSummary struct {
+	// mu guards reads/writes to the `state` field
+	mu              sync.Mutex
+	tasks           map[string]*TaskExecutionSummary // key is a taskID
+	profileFilename string
+
+	// These get serialized to JSON
+	success   int
+	failure   int
+	cached    int
+	attempted int
+	startedAt time.Time
+	endedAt   time.Time
+}
+
+// MarshalJSON munges the executionSummary into a format we want
+// We'll use an anonmyous, private struct for this, so it's not confusingly duplicated.
+func (es *executionSummary) MarshalJSON() ([]byte, error) {
+	serializable := struct {
+		Success   int   `json:"success"`
+		Failure   int   `json:"failed"`
+		Cached    int   `json:"cached"`
+		Attempted int   `json:"attempted"`
+		StartTime int64 `json:"startTime"`
+		EndTime   int64 `json:"endTime"`
+	}{
+		StartTime: es.startedAt.UnixMilli(),
+		EndTime:   es.endedAt.UnixMilli(),
+		Success:   es.success,
+		Failure:   es.failure,
+		Cached:    es.cached,
+		Attempted: es.attempted,
+	}
+
+	return json.Marshal(&serializable)
+}
+
+// newExecutionSummary creates a executionSummary instance to track events in a `turbo run`.`
+func newExecutionSummary(start time.Time, tracingProfile string) *executionSummary {
+	if tracingProfile != "" {
+		chrometracing.EnableTracing()
+	}
+
+	return &executionSummary{
+		success:         0,
+		failure:         0,
+		cached:          0,
+		attempted:       0,
+		tasks:           make(map[string]*TaskExecutionSummary),
+		startedAt:       start,
+		profileFilename: tracingProfile,
+	}
+}
+
+// Run starts the Execution of a single task. It returns a function that can
+// be used to update the state of a given taskID with the executionEventName enum
+func (es *executionSummary) run(taskID string) (func(outcome executionEventName, err error), *TaskExecutionSummary) {
+	start := time.Now()
+	taskExecutionSummary := es.add(&executionEvent{
+		Time:   start,
+		Label:  taskID,
+		Status: targetBuilding,
+	})
+
+	tracer := chrometracing.Event(taskID)
+
+	// This function can be called with an enum and an optional error to update
+	// the state of a given taskID.
+	tracerFn := func(outcome executionEventName, err error) {
+		defer tracer.Done()
+		now := time.Now()
+		result := &executionEvent{
+			Time:     now,
+			Duration: now.Sub(start),
+			Label:    taskID,
+			Status:   outcome,
+		}
+		if err != nil {
+			result.Err = fmt.Errorf("running %v failed: %w", taskID, err)
+		}
+		// Ignore the return value here
+		es.add(result)
+	}
+
+	return tracerFn, taskExecutionSummary
+}
+
+func (es *executionSummary) add(event *executionEvent) *TaskExecutionSummary {
+	es.mu.Lock()
+	defer es.mu.Unlock()
+
+	var taskExecSummary *TaskExecutionSummary
+	if ts, ok := es.tasks[event.Label]; ok {
+		// If we already know about this task, we'll update it with the new event
+		taskExecSummary = ts
+	} else {
+		// If we don't know about it yet, init and add it into the parent struct
+		// (event.Status should always be `targetBuilding` here.)
+		taskExecSummary = &TaskExecutionSummary{startAt: event.Time}
+		es.tasks[event.Label] = taskExecSummary
+	}
+
+	// Update the Status, Duration, and Err fields
+	taskExecSummary.status = event.Status
+	taskExecSummary.err = event.Err
+	taskExecSummary.duration = event.Duration
+
+	switch {
+	case event.Status == TargetBuildFailed:
+		es.failure++
+		es.attempted++
+	case event.Status == TargetCached:
+		es.cached++
+		es.attempted++
+	case event.Status == TargetBuilt:
+		es.success++
+		es.attempted++
+	}
+
+	return es.tasks[event.Label]
+}
+
+// writeChromeTracing writes to a profile name if the `--profile` flag was passed to turbo run
+func writeChrometracing(filename string, terminal cli.Ui) error {
+	outputPath := chrometracing.Path()
+	if outputPath == "" {
+		// tracing wasn't enabled
+		return nil
+	}
+
+	name := fmt.Sprintf("turbo-%s.trace", time.Now().Format(time.RFC3339))
+	if filename != "" {
+		name = filename
+	}
+	if err := chrometracing.Close(); err != nil {
+		terminal.Warn(fmt.Sprintf("Failed to flush tracing data: %v", err))
+	}
+	cwdRaw, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	root, err := fs.GetCwd(cwdRaw)
+	if err != nil {
+		return err
+	}
+	// chrometracing.Path() is absolute by default, but can still be relative if overriden via $CHROMETRACING_DIR
+	// so we have to account for that before converting to turbopath.AbsoluteSystemPath
+	if err := fs.CopyFile(&fs.LstatCachedFile{Path: fs.ResolveUnknownPath(root, outputPath)}, name); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ crates/turborepo/src/main.rs
@@ -0,0 +1,87 @@
+use std::{
+    env::{consts, current_exe},
+    process,
+    process::Stdio,
+};
+
+use anyhow::Result;
+use dunce::canonicalize as fs_canonicalize;
+use log::{debug, error, trace};
+use turborepo_lib::{Args, Payload};
+
+fn run_go_binary(args: Args) -> Result<i32> {
+    // canonicalize the binary path to ensure we can find go-turbo
+    let turbo_path = fs_canonicalize(current_exe()?)?;
+    let mut go_binary_path = turbo_path.clone();
+    go_binary_path.pop();
+    #[cfg(windows)]
+    go_binary_path.push("go-turbo.exe");
+    #[cfg(not(windows))]
+    go_binary_path.push("go-turbo");
+
+    if go_binary_path.exists() {
+        debug!("Found go binary at {:?}", go_binary_path);
+    } else {
+        error!("Unable to find Go binary. Please report this issue at https://github.com/vercel/turbo/issues and include your package manager and version along with the following information:
+        os={os}
+        arch={arch}
+        turbo-version={turbo_version}
+        turbo-bin={turbo_bin}
+        go-turbo-bin={go_turbo_bin}
+        ",
+            os = consts::OS,
+            arch = consts::ARCH,
+            turbo_version = turborepo_lib::get_version(),
+            turbo_bin = turbo_path.display(),
+            go_turbo_bin = go_binary_path.display()
+        );
+        // return an error
+        return Err(anyhow::anyhow!(
+            "Failed to execute turbo (Unable to locate Go binary)."
+        ));
+    }
+
+    let serialized_args = serde_json::to_string(&args)?;
+    trace!("Invoking go binary with {}", serialized_args);
+    let mut command = process::Command::new(go_binary_path);
+    command
+        .arg(serialized_args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit());
+
+    let shared_child = shared_child::SharedChild::spawn(&mut command).unwrap();
+    let child_arc = std::sync::Arc::new(shared_child);
+
+    let child_arc_clone = child_arc.clone();
+    ctrlc::set_handler(move || {
+        // on windows, we can't send signals so just kill
+        // we are quiting anyways so just ignore
+        #[cfg(target_os = "windows")]
+        child_arc_clone.kill().ok();
+
+        // on unix, we should send a SIGTERM to the child
+        // so that go can gracefully shut down process groups
+        // SAFETY: we could pull in the nix crate to handle this
+        // 'safely' but nix::sys::signal::kill just calls libc::kill
+        #[cfg(not(target_os = "windows"))]
+        unsafe {
+            libc::kill(child_arc_clone.id() as i32, libc::SIGTERM);
+        }
+    })
+    .expect("handler set");
+
+    let exit_code = child_arc.wait()?.code().unwrap_or(2);
+
+    Ok(exit_code)
+}
+
+// This function should not expanded. Please add any logic to
+// `turborepo_lib::main` instead
+fn main() -> Result<()> {
+    let exit_code = match turborepo_lib::main() {
+        Payload::Rust(res) => res.unwrap_or(1),
+        Payload::Go(state) => run_go_binary(*state)?,
+    };
+
+    process::exit(exit_code)
+}
