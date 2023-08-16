94e7c7aff97e4823901a8f24d058e8d3e85d7bd6
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-08-16 21:36:44 +0100

    Initialize sammyfilly-patch-17

---
+++ crates/turbopack-core/src/issue/unsupported_module.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use turbo_tasks::Vc;
+use turbo_tasks_fs::FileSystemPath;
+
+use super::{Issue, IssueSeverity};
+
+#[turbo_tasks::value(shared)]
+pub struct UnsupportedModuleIssue {
+    pub file_path: Vc<FileSystemPath>,
+    pub package: String,
+    pub package_path: Option<String>,
+}
+
+#[turbo_tasks::value_impl]
+impl Issue for UnsupportedModuleIssue {
+    #[turbo_tasks::function]
+    fn severity(&self) -> Vc<IssueSeverity> {
+        IssueSeverity::Warning.into()
+    }
+
+    #[turbo_tasks::function]
+    fn category(&self) -> Vc<String> {
+        Vc::cell("resolve".to_string())
+    }
+
+    #[turbo_tasks::function]
+    fn title(&self) -> Vc<String> {
+        Vc::cell("Unsupported module".into())
+    }
+
+    #[turbo_tasks::function]
+    fn file_path(&self) -> Vc<FileSystemPath> {
+        self.file_path
+    }
+
+    #[turbo_tasks::function]
+    async fn description(&self) -> Result<Vc<String>> {
+        Ok(Vc::cell(match &self.package_path {
+            Some(path) => format!("The module {}{} is not yet supported", self.package, path),
+            None => format!("The package {} is not yet supported", self.package),
+        }))
+    }
+}
