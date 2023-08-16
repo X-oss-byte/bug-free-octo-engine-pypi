0578d7dcb9fe53010621801f096eb2e5c3565ec4
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-08-16 21:03:16 +0100

    Initialize wbinnssmith/flakey-integration-debug-main

---
+++ cli/integration_tests/link.t
@@ -0,0 +1,8 @@
+Setup
+  $ . ${TESTDIR}/setup.sh
+
+Link Test Run
+  $ ${TURBO} link --__test-run
+  Repository inference failed: Unable to find `turbo.json` or `package.json` in current path
+  Running command as global turbo
+  Link test run successful
---
+++ crates/turbopack-ecmascript/src/references/esm/mod.rs
@@ -0,0 +1,16 @@
+pub(crate) mod base;
+pub(crate) mod binding;
+pub(crate) mod dynamic;
+pub(crate) mod export;
+pub(crate) mod meta;
+pub(crate) mod module_id;
+pub(crate) mod module_item;
+
+pub use self::{
+    base::{EsmAssetReference, EsmAssetReferenceVc},
+    binding::{EsmBinding, EsmBindingVc},
+    dynamic::{EsmAsyncAssetReference, EsmAsyncAssetReferenceVc},
+    export::{EsmExports, EsmExportsVc},
+    meta::{ImportMetaBinding, ImportMetaBindingVc, ImportMetaRef, ImportMetaRefVc},
+    module_item::{EsmModuleItem, EsmModuleItemVc},
+};
