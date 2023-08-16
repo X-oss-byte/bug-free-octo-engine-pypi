34a7ec0b59b9e547fb95fd6fcdec9027ef51237f
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-08-16 21:36:10 +0100

    Initialize snyk-upgrade-b7854e707581ffcd8a3af599745fb55d

---
+++ crates/turbopack-env/src/issue.rs
@@ -0,0 +1,33 @@
+use turbo_tasks::Vc;
+use turbo_tasks_fs::FileSystemPath;
+use turbopack_core::issue::Issue;
+
+/// An issue that occurred while resolving the parsing or evaluating the .env.
+#[turbo_tasks::value(shared)]
+pub struct ProcessEnvIssue {
+    pub path: Vc<FileSystemPath>,
+    pub description: Vc<String>,
+}
+
+#[turbo_tasks::value_impl]
+impl Issue for ProcessEnvIssue {
+    #[turbo_tasks::function]
+    fn title(&self) -> Vc<String> {
+        Vc::cell("Error loading dotenv file".to_string())
+    }
+
+    #[turbo_tasks::function]
+    fn category(&self) -> Vc<String> {
+        Vc::cell("parse".to_string())
+    }
+
+    #[turbo_tasks::function]
+    fn file_path(&self) -> Vc<FileSystemPath> {
+        self.path
+    }
+
+    #[turbo_tasks::function]
+    fn description(&self) -> Vc<String> {
+        self.description
+    }
+}
