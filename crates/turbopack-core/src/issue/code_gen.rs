7e4c1b47ac60025b1e9fea2c9d50da61e1ecd614
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-08-16 21:37:02 +0100

    Initialize snyk-upgrade-40030ab2fbd69e9f6e12c9ab7c5811e8

---
+++ crates/turbopack-core/src/issue/code_gen.rs
@@ -0,0 +1,40 @@
+use turbo_tasks::Vc;
+use turbo_tasks_fs::FileSystemPath;
+
+use super::{Issue, IssueSeverity};
+
+#[turbo_tasks::value(shared)]
+pub struct CodeGenerationIssue {
+    pub severity: Vc<IssueSeverity>,
+    pub path: Vc<FileSystemPath>,
+    pub title: Vc<String>,
+    pub message: Vc<String>,
+}
+
+#[turbo_tasks::value_impl]
+impl Issue for CodeGenerationIssue {
+    #[turbo_tasks::function]
+    fn severity(&self) -> Vc<IssueSeverity> {
+        self.severity
+    }
+
+    #[turbo_tasks::function]
+    fn title(&self) -> Vc<String> {
+        self.title
+    }
+
+    #[turbo_tasks::function]
+    fn category(&self) -> Vc<String> {
+        Vc::cell("code generation".to_string())
+    }
+
+    #[turbo_tasks::function]
+    fn file_path(&self) -> Vc<FileSystemPath> {
+        self.path
+    }
+
+    #[turbo_tasks::function]
+    fn description(&self) -> Vc<String> {
+        self.message
+    }
+}
